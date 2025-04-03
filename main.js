8507dbffab2d3a5a1fbeee0f7cf7d5dad4b901da
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-04-03 17:38:53 +0200

    leere Layer control erzeugt

--- main.js
+++ main.js
@@ -17,6 +17,13 @@ L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{
     attribution: 'Hintergrundkarte: <a href="https://www.basemap.at">basemap.at</a>'
 }).addTo(map);
 
+// Layercontrol
+L.control.layers({
+
+}, {
+
+}).addTo(map);
+
 // Maßstab
 L.control.scale({
     imperial: false,
