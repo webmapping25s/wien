730e9c177194d9da01edde97317b09273089482e
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-04-03 18:02:49 +0200

    GeoJSON Daten in die Overlays schreiben

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ async function loadSights(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
-    }).addTo(map);
+    }).addTo(overlays.sights);
 }
 
 // Touristische Kraftfahrlinien Liniennetz Vienna Sightseeing Linie Wien
@@ -56,7 +56,7 @@ async function loadLines(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
-    }).addTo(map);
+    }).addTo(overlays.lines);
 }
 
 // Touristische Kraftfahrlinien Haltestellen Vienna Sightseeing Linie Standorte Wien
@@ -67,7 +67,7 @@ async function loadStops(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
-    }).addTo(map);
+    }).addTo(overlays.stops);
 }
 
 // Fußgängerzonen
@@ -78,7 +78,7 @@ async function loadZones(url) {
     //console.log(jsondata);
     L.geoJSON(jsondata, {
         attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
-    }).addTo(map);
+    }).addTo(overlays.zones);
 }
 
 // GeoJSON laden und visualisieren
