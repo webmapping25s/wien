8ff527662a93b34325c418da5fbf9af75d877b8e
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-04-10 17:44:05 +0200

    Hotel Icons nach Sternen implementiert

--- main.js
+++ main.js
@@ -135,7 +135,34 @@ async function loadHotels(url) {
     let jsondata = await response.json();
     //console.log(jsondata);
     L.geoJSON(jsondata, {
-        attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
+        attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>",
+        pointToLayer: function(feature, latlng) {
+            //console.log(feature.properties.KATEGORIE_TXT);
+            let iconName;
+
+            if (feature.properties.KATEGORIE_TXT == "1*") {
+                iconName = "hotel_1stars.png";
+            } else if (feature.properties.KATEGORIE_TXT == "2*") {
+                iconName = "hotel_2stars.png";
+            } else if (feature.properties.KATEGORIE_TXT == "3*") {
+                iconName = "hotel_3stars.png";
+            } else if (feature.properties.KATEGORIE_TXT == "4*") {
+                iconName = "hotel_4stars.png";
+            } else if (feature.properties.KATEGORIE_TXT == "5*") {
+                iconName = "hotel_5stars.png";
+            } else {
+                iconName = "hotel_0stars.png";
+            }
+            //console.log(iconName);
+
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `icons/${iconName}`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            });
+        }
     }).addTo(overlays.hotels);
 }
 
