488acd2dc6d899a5c6f8e2e966f0c9c3e7135cd3
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-04-10 15:47:40 +0200

    Fußgängerzonen gestyled

--- main.js
+++ main.js
@@ -82,7 +82,16 @@ async function loadZones(url) {
     let jsondata = await response.json();
     //console.log(jsondata);
     L.geoJSON(jsondata, {
-        attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
+        attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>",
+        style: function (feature) {
+            console.log(feature);
+            return {
+                color: "#F012BE",
+                weight: 1,
+                opacity: 0.4,
+                fillOpacity: 0.1,
+            }
+        }
     }).addTo(overlays.zones);
 }
 
